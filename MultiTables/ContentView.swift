2cd6d64be9a667903c5c6084171a2d5d1a03311e
Tristin Smith <tsmithassoc@gmail.com>
2024-06-17 19:25:55 -0400

    Initial Commit

---
+++ MultiTables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiTables
+//
+//  Created by Tristin Smith on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiTables/MultiTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTablesApp.swift
+//  MultiTables
+//
+//  Created by Tristin Smith on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
